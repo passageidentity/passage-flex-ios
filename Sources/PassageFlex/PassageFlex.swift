c0fa6aab3b662e102d28f3f8d932e56ee8e0f1d3
Ricky Padilla <rickycpadilla@gmail.com>
2024-03-08 10:58:07 -0700

    chore(PSG-3663): added some inline docs

--- Sources/PassageFlex/PassageFlex.swift
+++ Sources/PassageFlex/PassageFlex.swift
@@ -1,15 +1,36 @@
 import Foundation
 
+/// Use `PassageFlex` to easily add passkeys to your existing authentication system on Apple devices.
 public struct PassageFlex {
     
+    /// The base struct for utilizing Apple's native passkey APIs and Passage Flex APIs together.
     public struct Passkey {
         
+        /// Checks if the user's device supports passkeys.
         public static let isSupported = Utilities.deviceOSSupportsPasskeys
         
+        
+        /// Registers a new passkey.
+        ///
+        /// Prompts the user associated with the provided Passage `transactionId` to create and register a new
+        /// passkey for use with your app.
+        ///
+        /// - Parameters:
+        ///   - transactionId: The Passage transaction id provided by your app's server.
+        ///   - authenticatorAttachment: (Optional) The type of authentication that will be used in this
+        ///   WebAuthN flow request. Defaults to `.platform`. Use `.cross-platform` for physical security
+        ///   key registration.
+        ///
+        /// - Returns: A single-use "nonce" from Passage server to be exchanged for an authentication token on
+        /// your app's server.
+        ///
+        /// - Throws: `PassagePasskeyAuthorizationError` when Apple passkey authorization fails.
+        /// `PassageRegisterPasskeyServerError` when the Passage server returns an error.
+        /// `PassageConfigurationError` when there was a problem with you app's configuration.
         @available(iOS 16.0, *)
         public static func register(
             with transactionId: String,
-            authenticatorAttachment: AuthenticatorAttachment? = nil
+            authenticatorAttachment: AuthenticatorAttachment = .platform
         ) async throws -> String {
             // Get Passage App ID from Passage.plist
             let appId = try Utilities.getAppId()
@@ -43,8 +64,23 @@ public struct PassageFlex {
             return finishResponse.nonce
         }
         
+        /// Authenticates with a passkey.
+        ///
+        /// Prompts the user to select a passkey for authentication for your app. If a Passage `transactionId` is provided,
+        /// this method will attempt to show only passkeys associated with that user account.
+        ///
+        /// - Parameters:
+        ///   - transactionId: (Optional) The Passage transaction id provided by your app's server.
+        ///
+        /// - Returns: A single-use "nonce" from Passage server to be exchanged for an authentication token on
+        /// your app's server.
+        ///
+        /// - Throws: `PassagePasskeyAuthorizationError` when Apple passkey authorization fails.
+        /// `PassageAuthenticatePasskeyServerError` when the Passage server returns an error.
+        /// `PassageConfigurationError` when there was a problem with you app's configuration.
         @available(iOS 16.0, *)
         public static func authenticate(with transactionId: String? = nil) async throws -> String {
+            let thing = try await Passkey.register(with: "")
             // Get Passage App ID from Passage.plist
             let appId = try Utilities.getAppId()
             // Request an Assertion Start Handshake from Passage server
--- Sources/PassageFlex/PassagePasskeyAuthorizationError.swift
+++ Sources/PassageFlex/PassagePasskeyAuthorizationError.swift
@@ -4,4 +4,18 @@ public enum PassagePasskeyAuthorizationError: Error {
     case userCanceled
     case failed
     case unknown
+    
+    var description: String {
+        switch self {
+        case .userCanceled:
+            return "The user canceled the request."
+        case .failed:
+            return "The authorization request failed."
+        case .unknown:
+            return "An unknown authorization error occurred."
+        }
+    }
+    
 }
+
+
